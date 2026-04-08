d00f68239dcb56ede2b50ebcfc558b6271fadf63
Yashvardhan Singh <yashvs2503@gmail.com>
2026-04-08 10:07:55 +0530

    Update registration to include email field

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ('username', 'role')
+        fields = ('username', 'email', 'role')
